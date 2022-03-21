23bba2368ea029e9d40fccd41800a9f03cfa2062
TheLBOstrich <102107527+TheLBOstrich@users.noreply.github.com>
2022-03-21 15:51:46 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -36,3 +36,22 @@ let mySprite2 = sprites.create(img`
     . . . . . f f b b f f . . . . . 
     `, SpriteKind.Player)
 controller.player2.moveSprite(mySprite2)
+let mySprite3 = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.player3.moveSprite(mySprite3, 100, 100)
